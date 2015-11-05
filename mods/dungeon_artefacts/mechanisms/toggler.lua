cae53d3389a47f3351f16785e667dadcfd340611
DonBatman <serfdon@gmail.com>
2015-11-05 15:09:07 -0800

    Added item drop blocks for rods. Added rod rooms

--- mods/dungeon_artefacts/mechanisms/toggler.lua
+++ mods/dungeon_artefacts/mechanisms/toggler.lua
@@ -7,6 +7,7 @@
 mechanisms.toggler_alias_table = {}
 
 mechanisms.toggler_alias_table["mechanisms:toggler_on"] = "mechanisms:toggler"
+mechanisms.toggler_alias_table["mechanisms:timed_toggler_on"] = "mechanisms:timed_toggler"
 
 
 mechanisms.register_punchstate("mechanisms:toggler", {
@@ -26,11 +27,44 @@ mechanisms.register_punchstate("mechanisms:toggler", {
 
 	on_mark_node = function(punchdata, node)
 		punchdata.name = mechanisms.toggler_alias_table[node.name] or node.name
-		print("added node at " .. minetest.pos_to_string(punchdata))
 		return true
 	end
 })
 
+
+mechanisms.register_punchstate("mechanisms:switcher", {
+	on_unmark_node = function(punchdata, node)
+		-- if the node punched is toggled already, remove it
+		-- otherwise toggle it (allowing us to save the node as off)
+		local meta = minetest.get_meta(punchdata)
+		local disabled = meta:get_int("disabled")
+		if disabled == 1 then
+		   meta:set_int("disabled", nil)
+		   return true -- remove mark
+		else
+		   meta:set_int("disabled", 1)
+		   return false, "mechanisms_mark_off.png" -- do not remove mark, mark it as off
+		end
+	end,
+
+	on_mark_node = function(punchdata, node)
+		-- only mark mechanisms
+		local def = minetest.registered_nodes[node.name]
+		if def and def.groups and def.groups.mechanism then
+			return true, "mechanisms_mark_on.png" -- mark it as on
+		end
+		return false
+	end,
+
+	get_mark_texture = function(pos)
+		local meta = minetest.get_meta(pos)
+		if meta and meta:get_int("disabled") == 0 then
+			return "mechanisms_mark_on.png"
+		else
+			return "mechanisms_mark_off.png"
+		end
+	end
+})
 
 function toggle_nodes(pos, toggler)
 	local meta = minetest.get_meta(pos)
@@ -51,53 +85,219 @@ function toggle_nodes(pos, toggler)
 	end
 end
 
+function switch_mechanisms(pos, toggler)
+	local meta = minetest.get_meta(pos)
+	local switching = meta:get_string("switching")
+
+	switching = switching and minetest.deserialize(switching)
+	if switching then
+		-- add a delay to prevent lockups
+		minetest.after(1, function()
+			for k,v in pairs(switching) do
+				local p = mechanisms.relative_to_absolute(toggler, pos, v)
+				local meta = minetest.get_meta(p)
+				local disabled = meta:get_int("disabled")
+				if disabled ~= 0 then
+					meta:set_int("disabled", 1)
+				else
+					meta:set_int("disabled", nil)
+				end
+				
+				local node = minetest.get_node(p)
+				local def = node and minetest.registered_nodes[node.name]
+				if def and def.on_switch then
+					def.on_switch(p, node, disabled ~= 0, meta)
+				end
+			end
+		end)
+	end
+end
+
+
+function toggler_save_punchstate(state, node, meta_field_name)
+	local pos = minetest.string_to_pos(state.id)
+	local meta = minetest.get_meta(pos)
+	node = node or minetest.get_node(pos)
+	
+	local togglenodes = {}
+	for k,v in pairs(state.nodes) do
+		local node = mechanisms.absolute_to_relative(node, pos, v)
+		node.name = v.name
+		table.insert(togglenodes, node)
+	end
+	meta:set_string(meta_field_name or "togglenodes", minetest.serialize(togglenodes))
+	
+	return #togglenodes
+end
+
+function toggler_load_punchstate(pos, node, meta_field_name, punchstate_name)
+	local meta = minetest.get_meta(pos)
+	local togglenodes = meta:get_string(meta_field_name or "togglenodes")
+	local punchs = {}
+	node = node or minetest.get_node(pos)
+
+	togglenodes = togglenodes and minetest.deserialize(togglenodes)
+	if togglenodes then
+	   for k,v in pairs(togglenodes) do
+		  local node = mechanisms.relative_to_absolute(node, pos, v)
+		  node.name = v.name
+		  table.insert(punchs, node)
+	   end
+	else
+	   togglenodes = { }
+	end
+	local punchstate = {
+		name = punchstate_name or "mechanisms:toggler",
+		id = minetest.pos_to_string(pos),
+		nodes = punchs,
+	}
+	return togglenodes, punchstate
+end
+
+
 function toggle_punch_node_selection(pos, node, player)
 	local name = player:get_player_name()
 
 	local state = mechanisms.end_player_punchstate(name, "mechanisms:toggler")
 	if state then
 		-- A punch state is already defined, save it!
-		pos = minetest.string_to_pos(state.id)
-		local meta = minetest.get_meta(pos)
+	   local count = toggler_save_punchstate(state, node)
+		minetest.chat_send_player(name, "Saved " .. count .. " toggling nodes")
+		minetest.log("action", name .. "saved toggler data with " .. count .. " toggleable nodes")
 
-		local togglenodes = {}
-		for k,v in pairs(state.nodes) do
-			local node = mechanisms.absolute_to_relative(node, pos, v)
-			node.name = v.name
-			table.insert(togglenodes, node)
-		end
-
-		minetest.chat_send_player(name, "Saved " .. #togglenodes .. " toggling nodes")
-		minetest.log("action", name .. "saved toggler data with " .. #togglenodes .. " toggleable nodes")
-		meta:set_string("togglenodes", minetest.serialize(togglenodes))
 	else
 		-- no punchstate defined, create it!
-		local meta = minetest.get_meta(pos)
-		local togglenodes = meta:get_string("togglenodes")
-		local punchs = {}
-
-		togglenodes = togglenodes and minetest.deserialize(togglenodes)
-		if togglenodes then
-			for k,v in pairs(togglenodes) do
-				local node = mechanisms.relative_to_absolute(node, pos, v)
-				node.name = v.name
-				table.insert(punchs, node)
-			end
-		else
-			togglenodes = { }
-		end
-
-		minetest.chat_send_player(name, "Node toggler edit mode! Punch nodes to assign them to this toggler,"
-			.." then right click again with the tome to save status. " .. #togglenodes .. " nodes are currently assigned.")
+	   local togglenodes, punchstate = toggler_load_punchstate(pos, node)
+	   mechanisms.begin_player_punchstate(name, punchstate)
+	   
+	   minetest.chat_send_player(name, "Node toggler edit mode! "..
+								 "Punch nodes to assign them to this toggler, "..
+								 "then right click again with the tome to save status. "..
+								 #togglenodes .. " nodes are currently assigned.")
 		minetest.log("action", "Loading toggler data with " .. #togglenodes .. " toggleable nodes")
-
-		mechanisms.begin_player_punchstate(name, {
-			name = "mechanisms:toggler",
-			id = minetest.pos_to_string(pos),
-			nodes = punchs,
-		})
 	end
 end
+
+-------------------
+-- Formspecs
+-------------------
+local formspec_context = {}
+
+function toggler_edit_timed(pos, node, player)
+	local name = player:get_player_name()
+
+	local state = mechanisms.end_player_punchstate(name, "mechanisms:toggler")
+	if state then
+		-- A punch state is already defined, save it!
+	    local count = toggler_save_punchstate(state)
+		minetest.chat_send_player(name, "Saved " .. count .. " toggling nodes")
+		minetest.log("action", name .. "saved toggler data with " .. count .. " toggleable nodes")
+	else
+		formspec_context[name] = {
+			pos = pos,
+		}
+		minetest.show_formspec(name, "mechanisms:toggler",
+							   "size[4,6]" ..
+								  "label[0,0; Timed Toggler]"..
+								  "textarea[0.5,1;3,2;;"..
+								  "This toggler is timed! when activated it'll start ticking, "..
+								  "after a given timeout it'll toggle back to the original state.\n"..
+								  "You can set a group of nodes that will disappear/appear when "..
+								  "the toggler state changes."..
+								  ";]"..
+								  "field[0.7,3.5.5;3,1;timeout;Timeout;10]" ..
+								  "button_exit[0.5,4.5;3,1;exit;Save & exit]" ..
+								  "button_exit[0.5,5.5;3,1;toggledit;Set nodes to toggle (punch them!)]")
+	end
+end
+
+function toggler_edit_switcher(pos, node, player)
+	local name = player:get_player_name()
+
+	local state = mechanisms.end_player_punchstate(name, "mechanisms:toggler")
+	if state then
+		-- A punch state is already defined, save it!
+	    local count = toggler_save_punchstate(state, node, "togglenodes")
+		minetest.chat_send_player(name, "Saved " .. count .. " toggling nodes")
+		minetest.log("action", name .. "saved toggler data with " .. count .. " toggleable nodes")
+		
+	else
+		state = mechanisms.end_player_punchstate(name, "mechanisms:switcher")
+		if state then 
+			-- A punch state is already defined, save it!
+			local count = toggler_save_punchstate(state, node, "switching")
+			minetest.chat_send_player(name, "Saved " .. count .. " switching mechanisms")
+			minetest.log("action", name .. "saved toggler data with " .. count .. " switcheable mechanisms")
+		else
+			formspec_context[name] = {
+				pos = pos,
+				node = node,
+			}
+			minetest.show_formspec(name, "mechanisms:toggler",
+								   "size[4,6]" ..
+									   "label[0,0; Toggler / Switcher]"..
+									   "textarea[0.5,1;4,2;;"..
+									   "This is both a toggler and a switcher!\n"..
+									   "You can set mechanisms that will get switched on/off when this mechanism "..
+									   "is activated. Switched off mechanisms might not carry out any action even "..
+									   "when activated.\n"..
+									   "You can also set a group of nodes to be toggled, these nodes will disappear/appear "..
+									   "when this mechanism is activated."..
+									   ";]"..
+									   "button_exit[0.5,4.5;3,1;switchedit;Set mechanisms to switch]" ..
+									   "button_exit[0.5,5.5;3,1;toggledit;Set nodes to toggle]")
+		end
+	end
+end
+-- Register callback
+minetest.register_on_player_receive_fields(function(player, formname, fields)
+	if formname == "mechanisms:toggler" then
+
+	   local name = player:get_player_name()
+		local context = formspec_context[name]
+
+		if not context or not context.pos then
+		   return false
+		end
+		local pos = context.pos
+		
+		if fields.timeout then
+			local meta = minetest.get_meta(pos)
+			meta:set_int("timeout",fields.timeout)
+			minetest.chat_send_player(name, "Saved timeout of " .. fields.timeout .. " seconds")
+		end
+		if fields.toggledit then
+			local togglenodes, punchstate = toggler_load_punchstate(pos)
+			mechanisms.begin_player_punchstate(name, punchstate)
+
+			minetest.chat_send_player(name, "Node toggler edit mode! Punch nodes to assign them to this toggler,"
+									  .." then right click again with the tome to save status. "
+									  .. #togglenodes .. " nodes are currently assigned.")
+			minetest.log("action", "Loading toggler data with " .. #togglenodes .. " toggleable nodes")
+		
+		elseif fields.switchedit then
+		   	local switchmechs, punchstate = toggler_load_punchstate(pos, context.node, "switching", "mechanisms:switcher")
+			mechanisms.begin_player_punchstate(name, punchstate)
+
+			minetest.chat_send_player(name, "Mechanism switcher edit mode! Punch mechanisms to assign them to this toggler,"
+									  .." then right click again with the tome to save status. "
+									  .. #switchmechs .. " mechanisms are currently assigned.")
+			minetest.log("action", "Loading toggler data with " .. #switchmechs .. " switching mechanisms")
+
+		   
+		end
+		formspec_context[name] = nil
+	else
+	   return false
+	end
+	-- Return true to stop other minetest.register_on_player_receive_fields
+	-- from receiving this submission.
+	return true
+end)
+
+-------------------
+-- Nodes
+-------------------
 
 minetest.register_node("mechanisms:toggler", {
 	description = "Dungeon Button",
@@ -128,14 +328,13 @@ minetest.register_node("mechanisms:toggler", {
 	groups = { creative_breakable = 1 },
 	on_rightclick = function (pos, node, player, itemstack, pointed_thing)
 		-- If the player is holding the Tome of DungeonMaking, allow setup
-		if itemstack.name == "dmaking:tome" then
+		if itemstack:get_name() == "dmaking:tome" then
 			toggle_punch_node_selection(pos, node, player)
 
 		else
 			minetest.swap_node(pos, {name = "mechanisms:toggler_on", param2=node.param2})
 			minetest.sound_play("default_hard_footstep", {pos=pos})
 			toggle_nodes(pos, node)
-			--minetest.after(1, mesecon.button_turnoff, pos)
 		end
 	end,
 	sounds = default.node_sound_stone_defaults(),
@@ -177,7 +376,6 @@ minetest.register_node("mechanisms:toggler_on", {
 			minetest.swap_node(pos, {name = "mechanisms:toggler", param2=node.param2})
 			minetest.sound_play("default_hard_footstep", {pos=pos})
 			toggle_nodes(pos, node)
-			--minetest.after(1, mesecon.button_turnoff, pos)
 		end
 	end,
 	groups = {creative_breakable=1, not_in_creative_inventory=1},
@@ -215,3 +413,233 @@ else
 		sunlight_propagates = true,
 	})
 end
+
+
+
+minetest.register_node("mechanisms:timed_toggler", {
+	description = "Timed Dungeon Button",
+	drawtype = "nodebox",
+	tiles = {
+	"default_stone.png",
+	"default_stone.png",
+	"default_stone.png",
+	"default_stone.png",
+	"default_stone.png",
+	"dungeontest_wall_decor.png",
+	},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	walkable = false,
+	sunlight_propagates = true,
+	selection_box = {
+		type = "fixed",
+		fixed = { -6/16, -6/16, 5/16, 6/16, 6/16, 8/16 },
+	},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{ -6/16, -6/16, 7/16, 6/16, 6/16, 8/16 },	-- the thin plate behind the button
+			{ -4/16, -4/16, 5/16, 4/16, 4/16, 7/16 },	-- the button itself
+		}
+	},
+	groups = { creative_breakable = 1, mechanism = 1, },
+	on_rightclick = function (pos, node, player, itemstack, pointed_thing)
+		-- If the player is holding the Tome of DungeonMaking, allow setup
+		if itemstack:get_name() == "dmaking:tome" then
+			toggler_edit_timed(pos, node, player)
+
+		else
+			minetest.swap_node(pos, {name = "mechanisms:timed_toggler_on", param2=node.param2})
+			minetest.sound_play("default_hard_footstep", {pos=pos})
+			local meta = minetest.get_meta(pos)
+			local timeout = meta:get_int("timeout")
+			toggle_nodes(pos, node, meta)
+			if timeout <= 0 then timeout = 10 end
+
+			local ticking = minetest.sound_play("mechanisms_ticking", {pos = pos,5, loop=true})
+			minetest.get_node_timer(pos):start(timeout)
+			minetest.after(timeout, function()
+				minetest.sound_stop(ticking)
+			end)
+		end
+	end,
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("mechanisms:timed_toggler_on", {
+	description = "Timed Dungeon Button",
+	drawtype = "nodebox",
+	tiles = {
+		"default_stone.png",
+		"default_stone.png",
+		"default_stone.png",
+		"default_stone.png",
+		"default_stone.png",
+		"dungeontest_wall_decor.png"
+		},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	walkable = false,
+	light_source = default.LIGHT_MAX-7,
+	sunlight_propagates = true,
+	selection_box = {
+		type = "fixed",
+		fixed = { -6/16, -6/16, 5/16, 6/16, 6/16, 8/16 }
+	},
+	node_box = {
+		type = "fixed",
+			fixed = {
+				{ -6/16, -6/16,  7/16, 6/16, 6/16, 8/16 },
+				{ -4/16, -4/16,  6.8/16, 4/16, 4/16, 7/16 }
+			}
+	},
+	on_timer = function (pos, elapsed)
+	   local node = minetest.get_node(pos)
+	   node.name = "mechanisms:timed_toggler"
+	   minetest.swap_node(pos, node)
+	   minetest.sound_play("default_hard_footstep", {pos=pos})
+	   toggle_nodes(pos, node)
+	end,
+	groups = {creative_breakable=1, mechanism=1, not_in_creative_inventory=1},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+
+-- Dungeon lever
+minetest.register_node("mechanisms:lever_up", {
+	description = "Dungeon Lever 1",
+	drawtype = "nodebox",
+	tiles = {
+			"mechanisms_switch_front_up.png",
+			"mechanisms_switch_back.png",
+			"mechanisms_switch_side_up.png^[transformFX",
+			"mechanisms_switch_side_up.png",
+			"mechanisms_switch_top.png",
+			"mechanisms_switch_top.png^[transformFY",
+			},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	groups = {creative_breakable=1, mechanism=1},
+	is_ground_content = false,
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.3125, -0.5, -0.3125, 0.3125, -0.375, 0.3125},
+			{-0.0625, -0.5, -0.0625, 0.0625, -0.25, 0.0625},
+			{-0.0625, -0.3125, 0, 0.0625, -0.1875, 0.125},
+			{-0.0625, -0.25, 0.0625, 0.0625, -0.125, 0.1875},
+			{-0.0625, -0.1875, 0.125, 0.0625, -0.0625, 0.25},
+			{-0.0625, -0.125, 0.1875, 0.0625, -0, 0.3125},
+			{-0.0625, -0.0625, 0.25, 0.0625, 0.0625, 0.375},
+			{0.0625, -0.375, -0.125, 0.125, -0.1875, 0.125},
+			{-0.125, -0.5, -0.125, -0.0625, -0.1875, 0.125},
+		--[[
+			{-0.3125, -0.3125, 0.375, 0.3125, 0.3125, 0.5},
+			{-0.0625, -0.0625, 0.25, 0.0625, 0.0625, 0.5},
+			{-0.0625, 0, 0.1875, 0.0625, 0.125, 0.3125},
+			{-0.0625, 0.0625, 0.125, 0.0625, 0.1875, 0.25},
+			{-0.0625, 0.125, 0.0625, 0.0625, 0.25, 0.1875},
+			{-0.0625, 0.1875, 0, 0.0625, 0.3125, 0.125},
+			{-0.0625, 0.25, -0.0625, 0.0625, 0.375, 0.0625},
+			{0.0625, -0.125, 0.1875, 0.125, 0.125, 0.375},
+			{-0.125, -0.125, 0.1875, -0.0625, 0.125, 0.375},
+		--]]
+		}
+	},
+	on_construct = function(pos)
+		local node = minetest.get_node(pos)
+		if node.param2 == 7 then
+			minetest.set_node(pos,{name = "mechanisms:lever_up", param2 = 6})
+		elseif node.param2 == 9 then
+			minetest.set_node(pos,{name = "mechanisms:lever_up", param2 = 8})
+		elseif node.param2 == 12 then
+			minetest.set_node(pos,{name = "mechanisms:lever_up", param2 = 15})
+		elseif node.param2 == 18 then
+			minetest.set_node(pos,{name = "mechanisms:lever_up", param2 = 17})
+		end
+	end,
+	on_rightclick = function(pos, node, player, itemstack, pointed_thing)
+		-- If the player is holding the Tome of DungeonMaking, allow setup
+		if itemstack:get_name() == "dmaking:tome" then
+			toggler_edit_switcher(pos, node, player)
+			
+		else
+			minetest.swap_node(pos, {name = "mechanisms:lever_down", param2=node.param2})
+			minetest.sound_play("default_hard_footstep", {pos=pos})
+			local meta = minetest.get_meta(pos)
+			if meta:get_int("disabled") == 0 then
+				toggle_nodes(pos, node)
+				switch_mechanisms(pos, node)
+			end
+		end
+	end,
+	-- -- Don't toggle this state, we'll consider this the initial (off) state 
+	-- on_switch = function(pos, node, disabled, meta)
+	--    toggle_nodes(pos, node)
+	--    switch_mechanisms(pos, node)
+	-- end,
+	on_place = minetest.rotate_node
+
+})
+--Switch Down
+minetest.register_node("mechanisms:lever_down", {
+	description = "Dungeon Lever",
+	drawtype = "nodebox",
+	tiles = {
+			"mechanisms_switch_front_down.png",
+			"mechanisms_switch_back.png",
+			"mechanisms_switch_side_down.png^[transformFX",
+			"mechanisms_switch_side_down.png",
+			"mechanisms_switch_top.png",
+			"mechanisms_switch_top.png^[transformFY",
+			},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	groups = {creative_breakable=1, mechanism=1, not_in_creative_inventory = 1},
+	is_ground_content = false,
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.3125, -0.5, -0.3125, 0.3125, -0.375, 0.3125},
+			{-0.0625, -0.5, -0.0625, 0.0625, -0.25, 0.0625},
+			{-0.0625, -0.3125, -0.125, 0.0625, -0.1875, -0},
+			{-0.0625, -0.25, -0.1875, 0.0625, -0.125, -0.0625},
+			{-0.0625, -0.1875, -0.25, 0.0625, -0.0625, -0.125},
+			{-0.0625, -0.125, -0.3125, 0.0625, -0, -0.1875},
+			{-0.0625, -0.0625, -0.375, 0.0625, 0.0625, -0.25},
+			{-0.125, -0.375, -0.125, -0.0625, -0.1875, 0.125},
+			{0.0625, -0.5, -0.125, 0.125, -0.1875, 0.125},
+		--[[
+			{-0.3125, -0.3125, 0.375, 0.3125, 0.3125, 0.5},
+			{-0.0625, -0.0625, 0.25, 0.0625, 0.0625, 0.5},
+			{-0.0625, -0.125, 0.1875, 0.0625, -0, 0.3125},
+			{-0.0625, -0.1875, 0.125, 0.0625, -0.0625, 0.25},
+			{-0.0625, -0.25, 0.0625, 0.0625, -0.125, 0.1875},
+			{-0.0625, -0.3125, 0, 0.0625, -0.1875, 0.125},
+			{-0.0625, -0.375, -0.0625, 0.0625, -0.25, 0.0625},
+			{-0.125, -0.125, 0.1875, -0.0625, 0.125, 0.375},
+			{0.0625, -0.125, 0.21875, 0.125, 0.125, 0.375},
+		--]]
+		}
+	},
+	on_rightclick = function(pos, node, player, itemstack, pointed_thing)
+		-- If the player is holding the Tome of DungeonMaking, allow setup
+		if itemstack:get_name() == "dmaking:tome" then
+			toggler_edit_switcher(pos, node, player)
+
+		else
+			minetest.swap_node(pos, {name = "mechanisms:lever_up", param2=node.param2})
+			minetest.sound_play("default_hard_footstep", {pos=pos})
+			local meta = minetest.get_meta(pos)
+			if meta:get_int("disabled") == 0 then
+				toggle_nodes(pos, node)
+				switch_mechanisms(pos, node)
+			end
+		end
+	end,
+	on_switch = function(pos, node, disabled, meta)
+	   toggle_nodes(pos, node)
+	   switch_mechanisms(pos, node)
+	end,
+	on_place = minetest.rotate_node
+})
--- mods/dungeon_magic/blocks.lua
+++ mods/dungeon_magic/blocks.lua
@@ -23,3 +23,65 @@ minetest.register_node("dungeon_magic:block_"..col..tool,{
 
 })
 end
+
+local function parti(pos)
+  	minetest.add_particlespawner(40, 1,
+		pos, pos,
+		{x=-5, y=-5, z=-5}, {x=5, y=5, z=5},
+		{x=-2, y=-2, z=-2}, {x=2, y=2, z=2},
+		0.2, 2,
+		0.2, 3,
+		false, "dungeon_magic_parti.png")
+end
+
+local items = {
+		{"rods:rod_of_blinking","blinking","Blinking Block"},
+		{"rods:rod_of_irrigation","irrigation","Irrigation Block"},
+		{"rods:rod_of_immolation","immolation","Immolation Block"},
+		}
+for i in ipairs (items) do
+	local itm = items[i][1]
+	local nam = items[i][2]
+	local des = items[i][3]
+
+minetest.register_node("dungeon_magic:"..nam.."_block",{
+	description = des,
+	tiles = {"dungeon_block_"..nam..".png"},
+	drawtype = "nodebox",
+	paramtype = "light",
+	drop = "",
+	light_source = 12,
+	groups = {creative_breakable=1, magic_sword = 1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, 0.3125, -0.3125, 0.5, 0.5},
+			{-0.5, -0.5, -0.5, -0.3125, 0.5, -0.3125},
+			{0.3125, -0.5, -0.5, 0.5, 0.5, -0.3125},
+			{0.3125, -0.5, 0.3125, 0.5, 0.5, 0.5},
+			{0.3125, -0.5, -0.3125, 0.5, -0.3125, 0.3125},
+			{-0.5, -0.5, -0.3125, -0.3125, -0.3125, 0.3125},
+			{-0.5, 0.3125, -0.3125, -0.3125, 0.5, 0.3125},
+			{0.3125, 0.3125, -0.3125, 0.5, 0.5, 0.3125},
+			{-0.3125, 0.3125, 0.3125, 0.3125, 0.5, 0.5},
+			{-0.3125, 0.3125, -0.5, 0.3125, 0.5, -0.3125},
+			{-0.3125, -0.5, -0.5, 0.3125, -0.3125, -0.3125},
+			{-0.3125, -0.5, 0.3125, 0.3125, -0.3125, 0.5},
+			{-0.3125, -0.3125, -0.3125, 0.3125, 0.3125, 0.3125},
+		}
+	},
+	on_destruct = function(pos)
+		minetest.spawn_item(pos, itm)
+		parti(pos)
+	end,
+})
+end
+minetest.register_node("dungeon_magic:teleport_fake",{
+	description = "FakeTeleport Block",
+	tiles = {{name="dungeon_teleport_ani_lime.png", animation={type="vertical_frames",aspect_w=16, aspect_h=16, length=0.5}}},
+	paramtype = "light",
+	drop = "",
+	light_source = 14,
+	groups = {creative_breakable=1},
+
+})
